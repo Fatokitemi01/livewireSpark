7e5e27403f08a4aa0bc320a18a008bd52476b5f9
Fatokitemi01 <fatokitemi01@gmail.com>
2022-08-07 22:01:38 +0000

    Update Welcome.blade.php
    Fixed Register and Login Page, with Checkout page to be working

--- livewireSpark/resources/views/welcome.blade.php
+++ livewireSpark/resources/views/welcome.blade.php
@@ -6,8 +6,22 @@
     <link href ="style.css" rel="stylesheet" type ="text/css">
     </head>
     <body>
-  <button id="checkout">CheckOut</button>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+  <button id="checkout">CheckOut</button>
+</div>
 <script>
 var stripe = Stripe("pk_test_51LOkgXDVWgydybi3mm6cYW121LFZRDx2Nxqtjpxhabjeuhfu0GgfbM0H82JsWKVNKPOoBr48xmT3CJSSgfAmBlfU00tykAfeLA")
 document.getElementById("checkout").addEventListener("click",function(){
